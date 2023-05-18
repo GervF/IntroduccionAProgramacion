58df63652b38a98f055d1cf27ff4d51016eb4669
GervF <gerfabreau@gmail.com>
2023-05-18 11:49:19 -0300

    ej while

---
+++ enct8/ejwhilejs/ej003while.js
@@ -0,0 +1,11 @@
+// 3. Suma de cifras.
+// Ingresar un número de cualquier cifra y mostrar,
+// la suma de sus cifras.
+
+let suma = 0;
+let contador = 1
+let control = 10
+let i = 1
+
+let numero = prompt("Ingrese un numero de cualquier cifra y le digo la suma de las cifras")
+while (numero < i ) {numero.toString().split('')}
