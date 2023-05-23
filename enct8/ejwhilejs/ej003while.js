808ad9381cfaac6dd3061a0e262517b6d4585709
GervF <gerfabreau@gmail.com>
2023-05-23 09:38:51 -0300

    ej004while correcciones

--- enct8/ejwhilejs/ej003while.js
+++ enct8/ejwhilejs/ej003while.js
@@ -3,9 +3,16 @@
 // la suma de sus cifras.
 
 let suma = 0;
-let contador = 1
+let contador = 0
 let control = 10
 let i = 1
 
-let numero = prompt("Ingrese un numero de cualquier cifra y le digo la suma de las cifras")
-while (numero < i ) {numero.toString().split('')}
+let numero = parseInt(prompt("Ingrese un numero de cualquier cifra y le digo la suma de las cifras"))
+
+while (numero > 0) {
+    digito = numero % 10
+    suma = suma + digito
+    numero = parseInt(numero / 10)
+}
+
+alert("La suma de las cifras es: " + suma)
---
+++ enct8/ejwhilejs/ej004while.js
@@ -0,0 +1,3 @@
+// 4. Número de cifras invertidas. - Ejm. (342 - 243)
+// Ingresar un número cualquiera de varias cifras y
+// devolver el mismo número con sus cifras invertidas.
